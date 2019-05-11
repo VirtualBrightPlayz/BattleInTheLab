c7351b9a2822467da1c82fddca7d04dba94e61e3
VirtualBrightPlayz <lego.trains.b@gmail.com>
2019-05-10 22:38:07 -0700

    v1.2
    stuff

--- Mod11/Mod11.cs
+++ Mod11/Mod11.cs
@@ -1,6 +1,5 @@
 ﻿using Smod2;
 using Smod2.Attributes;
-using GamemodeManager;
 using Smod2.Config;
 using System.Net;
 using System.Net.Sockets;
@@ -33,30 +32,7 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
         public override void Register()
         {
             this.AddEventHandlers(new Mod11EventHandler(this), Smod2.Events.Priority.Normal);
-            this.AddConfig(new ConfigSetting("battleroyale_weapons", new int[] { (int)ItemType.COM15, (int)ItemType.FRAG_GRENADE, (int)ItemType.MP4, (int)ItemType.P90 }, SettingType.NUMERIC_LIST, true, "A list of all the weapons that can spawn"));
-            GamemodeManager.GamemodeManager.RegisterMode(this, "44444444444444444444");
-
-            Dictionary<string, string> translations = new Dictionary<string, string>
-            {
-                { "MODEBR_YOU_ARE", "You are" },
-                { "MODEBR_DESCRIPTION", "Description" },
-                { "MODEBR_GOAL", "Goal" },
-
-                { "MODEBR_CLASSD", "Class-D Personnel" },
-                { "MODEBR_CLASSD_DESC", "You wake up in a random room." },
-                { "MODEBR_CLASSD_GOAL", "Escape, but try not to get killed by others." },
-
-                { "MODEBR_CHAOS_INSUGENCY", "Chaos Insurgency" },
-                { "MODEBR_CHAOS_INSUGENCY_DESC", "You have escaped the facility." },
-                { "MODEBR_CHAOS_INSUGENCY_GOAL", "Kill all Class-D Personnel, but not your crew.\nOr you could skrew around." },
-
-                { "CLASSD", "Dboi" }
-            };
-
-            foreach (KeyValuePair<string, string> translation in translations)
-            {
-                this.AddTranslation(new LangSetting(translation.Key, translation.Value, "gamemode_battleroyale"));
-            }
+            this.AddConfig(new ConfigSetting("battleroyale_weapons", new int[] { (int)ItemType.COM15, (int)ItemType.FRAG_GRENADE, (int)ItemType.MP4, (int)ItemType.P90 }, true, "A list of all the weapons that can spawn"));
         }
     }
 }
--- Mod11/Mod11EventHandler.cs
+++ Mod11/Mod11EventHandler.cs
@@ -3,8 +3,10 @@ using Smod2.API;
 using Smod2.EventHandlers;
 using Smod2.Events;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace VirtualBrightPlayz.SCPSL.Mod11
 {
@@ -35,31 +37,34 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
             //ev.ChangeRole = Smod2.API.Role.CHAOS_INSUGENCY;
         }
 
-        void GetRooms()
+        void GetRooms(bool lczd = false)
         {
             var lczrooms = GameObject.Find("LightRooms").transform;
-            var hczrooms = GameObject.Find("Heavy rooms").transform;
+            var hczrooms = GameObject.Find("HeavyRooms").transform;
             var ezrooms = GameObject.Find("EntranceRooms").transform;
             rooms = new List<Transform>();
-            for (int i = 0; i < lczrooms.childCount; i++)
+            if (!lczd)
             {
-                Transform room = lczrooms.GetChild(i);
-                Vector3 pos = new Vector3(lczrooms.position.x, room.position.y, lczrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("173"))
-                    rooms.Add(room);
+                for (int i = 0; i < lczrooms.childCount; i++)
+                {
+                    Transform room = lczrooms.GetChild(i);
+                    Vector3 pos = new Vector3(lczrooms.position.x, room.position.y, lczrooms.position.y);
+                    if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("173") && !room.name.Contains("Armory"))
+                        rooms.Add(room);
+                }
             }
             for (int i = 0; i < hczrooms.childCount; i++)
             {
                 Transform room = hczrooms.GetChild(i);
                 Vector3 pos = new Vector3(hczrooms.position.x, room.position.y, hczrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Tesla"))
+                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Tesla") && !room.name.Contains("Room3ar"))
                     rooms.Add(room);
             }
             for (int i = 0; i < ezrooms.childCount; i++)
             {
                 Transform room = ezrooms.GetChild(i);
                 Vector3 pos = new Vector3(ezrooms.position.x, room.position.y, ezrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Shelter") && !room.name.Contains("CollapsedTunnel"))
+                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Shelter") && !room.name.Contains("CollapsedTunnel") && !room.name.Contains("GateB") && !room.name.Contains("GateA"))
                     rooms.Add(room);
             }
         }
@@ -77,34 +82,14 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
             //    ammo7.Add(player.SteamId, player.GetAmmo(AmmoType.DROPPED_7));
             //    ammo9.Add(player.SteamId, player.GetAmmo(AmmoType.DROPPED_9));
             //}
-            if (rooms == null)
+            GameObject.FindObjectOfType<Broadcast>().CallRpcAddElement("<size=50>Welcome Subject D-[Redacted]</size>", 3, true);
+            GameObject.FindObjectOfType<Broadcast>().CallRpcAddElement("<size=20><color=#fca714>You have been selected to fight against other Class-D Personnel.\n" +
+                "This is a fight to the death, and you must be the last survivor in order to be released from the facility.\n" +
+                "Supplies have been placed around the facility, and are free to use.</color></size>", 10, true);
+            GameObject.FindObjectOfType<Broadcast>().CallRpcAddElement("<size=100>Good Luck!</size>", 2, true);
+            if (rooms == null || true)
             {
                 GetRooms();
-            }
-            var lczrooms = GameObject.Find("LightRooms").transform;
-            var hczrooms = GameObject.Find("Heavy rooms").transform;
-            var ezrooms = GameObject.Find("EntranceRooms").transform;
-            rooms = new List<Transform>();
-            for (int i = 0; i < lczrooms.childCount; i++)
-            {
-                Transform room = lczrooms.GetChild(i);
-                Vector3 pos = new Vector3(lczrooms.position.x, room.position.y, lczrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("173") && !room.name.Contains("Armory"))
-                    rooms.Add(room);
-            }
-            for (int i = 0; i < hczrooms.childCount; i++)
-            {
-                Transform room = hczrooms.GetChild(i);
-                Vector3 pos = new Vector3(hczrooms.position.x, room.position.y, hczrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Tesla") && !room.name.Contains("Room3ar"))
-                    rooms.Add(room);
-            }
-            for (int i = 0; i < ezrooms.childCount; i++)
-            {
-                Transform room = ezrooms.GetChild(i);
-                Vector3 pos = new Vector3(ezrooms.position.x, room.position.y, ezrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Shelter") && !room.name.Contains("CollapsedTunnel") && !room.name.Contains("GateB") && !room.name.Contains("GateA"))
-                    rooms.Add(room);
             }
             players = new List<string>();
             foreach (var player in mod11.Server.GetPlayers())
@@ -141,31 +126,15 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
 
         void IEventHandlerLCZDecontaminate.OnDecontaminate()
         {
-            var hczrooms = GameObject.Find("Heavy rooms").transform;
-            var ezrooms = GameObject.Find("EntranceRooms").transform;
-            rooms = new List<Transform>();
-            for (int i = 0; i < hczrooms.childCount; i++)
-            {
-                Transform room = hczrooms.GetChild(i);
-                Vector3 pos = new Vector3(hczrooms.position.x, room.position.y, hczrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Tesla"))
-                    rooms.Add(room);
-            }
-            for (int i = 0; i < ezrooms.childCount; i++)
-            {
-                Transform room = ezrooms.GetChild(i);
-                Vector3 pos = new Vector3(ezrooms.position.x, room.position.y, ezrooms.position.y);
-                if (room.name.StartsWith("Root_") && Vector3.Distance(pos, room.position) < MinDist && !room.name.Contains("Shelter") && !room.name.Contains("CollapsedTunnel"))
-                    rooms.Add(room);
-            }
+            GetRooms(true);
         }
 
         void IEventHandlerSpawn.OnSpawn(PlayerSpawnEvent ev)
         {
-            if (rooms == null)
+            /*if (rooms == null)
             {
                 GetRooms();
-            }
+            }*/
         }
 
         void IEventHandlerRoundEnd.OnRoundEnd(RoundEndEvent ev)
@@ -182,13 +151,15 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
                 if (player.TeamRole.Role == Role.CLASSD)
                     roles.Add(player.TeamRole.Role);
             }
-            if (roles.Count > 1)
+            if (roles.Count > 1 && !hasended)
             {
                 ev.Status = ROUND_END_STATUS.ON_GOING;
             }
             else
             {
+                //ev.Status = ROUND_END_STATUS.ON_GOING;
                 ev.Status = ROUND_END_STATUS.CI_VICTORY;
+                ev.Round.EndRound();
             }
         }
 
@@ -226,7 +197,11 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
             if (ev.Role == Role.CLASSD || ev.TeamRole.Team == Smod2.API.Team.SCP || ev.TeamRole.Team == Smod2.API.Team.NONE || ev.TeamRole.Team == Smod2.API.Team.TUTORIAL || ev.TeamRole.Team == Smod2.API.Team.SPECTATOR)
             { }
             else
-                ev.Role = Role.CHAOS_INSUGENCY;
+            {
+                ev.Player.ChangeRole(Role.CHAOS_INSURGENCY);
+                ev.Items.Add(ItemType.O5_LEVEL_KEYCARD);
+                GameObject.FindObjectOfType<Broadcast>().CallTargetAddElement(((GameObject)ev.Player.GetGameObject()).GetComponent<NetworkIdentity>().connectionToClient, "<color=#00ff00>Kill the Class-D Personnel, or not.</color>", 5, true);
+            }
         }
 
         void IEventHandlerUpdate.OnUpdate(UpdateEvent ev)
@@ -237,22 +212,26 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
                 if (player.TeamRole.Role == Role.CLASSD)
                     roles.Add(player.Name);
             }
+            if (roles.Count == 2 && !hasended && roundstarted && !GameObject.FindObjectOfType<AlphaWarheadController>().inProgress && !GameObject.FindObjectOfType<AlphaWarheadController>().detonated)
+            {
+                GameObject.FindObjectOfType<AlphaWarheadController>().StartDetonation();
+            }
             if (roles.Count == 1 && !hasended && roundstarted)
             {
+                GameObject.FindObjectOfType<Broadcast>().CallRpcAddElement("<size=70><color=#ff0000>Winner: " + roles[0] + "</color></size>", 10, true);
                 foreach (Player player in mod11.Server.GetPlayers())
                 {
                     player.SendConsoleMessage(roles[0] + " Wins!", "red");
                 }
-                mod11.Server.Round.EndRound();
                 hasended = true;
             }
             else if (roles.Count < 1 && !hasended && roundstarted)
             {
+                GameObject.FindObjectOfType<Broadcast>().CallRpcAddElement("<size=70><color=#ff0000>No one wins.</color></size>", 10, true);
                 foreach (Player player in mod11.Server.GetPlayers())
                 {
                     player.SendConsoleMessage("No one wins.", "red");
                 }
-                mod11.Server.Round.EndRound();
                 hasended = true;
             }
             /*foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Pickup"))
@@ -297,13 +276,17 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
                 {
                     foreach (var player in mod11.Server.GetPlayers())
                     {
-                        //mod11.Info(rooms.Count.ToString());
-                        int roomnumber = Random.Range(0, rooms.Count);
-                        Vector3 roompos = rooms[roomnumber].position;
-                        //plugin.Info(rooms[roomnumber].name);
-                        player.Teleport(new Smod2.API.Vector(roompos.x, roompos.y + 2f, roompos.z));
-                        player.GiveItem(Smod2.API.ItemType.ZONE_MANAGER_KEYCARD);
-                        players.Add(player.SteamId);
+                        if (player.SteamId == ev.Player.SteamId)
+                        {
+                            //mod11.Info(rooms.Count.ToString());
+                            int roomnumber = Random.Range(0, rooms.Count);
+                            Vector3 roompos = rooms[roomnumber].position;
+                            //plugin.Info(rooms[roomnumber].name);
+                            player.ChangeRole(Role.CLASSD);
+                            player.Teleport(new Smod2.API.Vector(roompos.x, roompos.y + 2f, roompos.z));
+                            player.GiveItem(Smod2.API.ItemType.ZONE_MANAGER_KEYCARD);
+                            players.Add(player.SteamId);
+                        }
                     }
                 }
             }
@@ -311,7 +294,7 @@ namespace VirtualBrightPlayz.SCPSL.Mod11
 
         void IEventHandlerPlayerHurt.OnPlayerHurt(PlayerHurtEvent ev)
         {
-            if (ev.Attacker.TeamRole.Role == Role.CHAOS_INSUGENCY && ev.Player.TeamRole.Role == Role.CHAOS_INSUGENCY)
+            if (ev.Attacker.TeamRole.Role == Role.CHAOS_INSURGENCY && ev.Player.TeamRole.Role == Role.CHAOS_INSURGENCY)
             {
                 ev.Damage = 0;
             }
